997324235b7a1fcc0811548b463fb732401fc384
JaviBehrens <115726458+JaviBehrens@users.noreply.github.com>
2022-11-17 09:39:35 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as Web3 from '@solana/web3.js';
 import * as fs from 'fs';
 import dotenv from 'dotenv';
+dotenv.config();
 
 async function initializeKeypair(
     connection: Web3.Connection
@@ -13,35 +14,6 @@ async function initializeKeypair(
       fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
   
       return signer;
-
-      async function airdropSolIfNeeded(
-        signer: Web3.Keypair,
-        connection: Web3.Connection
-      ) {
-        const balance = await connection.getBalance(signer.publicKey);
-        console.log('Current balance is', balance / Web3.LAMPORTS_PER_SOL, 'SOL');
-      
-        // 1 SOL should be enough for almost anything you wanna do
-        if (balance / Web3.LAMPORTS_PER_SOL < 1) {
-          // You can only get up to 2 SOL per request 
-          console.log('Airdropping 1 SOL');
-          const airdropSignature = await connection.requestAirdrop(
-            signer.publicKey,
-            Web3.LAMPORTS_PER_SOL
-          );
-      
-          const latestBlockhash = await connection.getLatestBlockhash();
-      
-          await connection.confirmTransaction({
-            blockhash: latestBlockhash.blockhash,
-            lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
-            signature: airdropSignature,
-          });
-      
-          const newBalance = await connection.getBalance(signer.publicKey);
-          console.log('New balance is', newBalance / Web3.LAMPORTS_PER_SOL, 'SOL');
-        }
-      }
     }
   
     const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];
@@ -50,7 +22,34 @@ async function initializeKeypair(
     return keypairFromSecret;
   }
 
-dotenv.config();
+  async function airdropSolIfNeeded(
+    signer: Web3.Keypair,
+    connection: Web3.Connection
+  ) {
+    const balance = await connection.getBalance(signer.publicKey);
+    console.log('Current balance is', balance / Web3.LAMPORTS_PER_SOL, 'SOL');
+  
+    // 1 SOL should be enough for almost anything you wanna do
+    if (balance / Web3.LAMPORTS_PER_SOL < 1) {
+      // You can only get up to 2 SOL per request 
+      console.log('Airdropping 1 SOL');
+      const airdropSignature = await connection.requestAirdrop(
+        signer.publicKey,
+        Web3.LAMPORTS_PER_SOL
+      );
+  
+      const latestBlockhash = await connection.getLatestBlockhash();
+  
+      await connection.confirmTransaction({
+        blockhash: latestBlockhash.blockhash,
+        lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+        signature: airdropSignature,
+      });
+  
+      const newBalance = await connection.getBalance(signer.publicKey);
+      console.log('New balance is', newBalance / Web3.LAMPORTS_PER_SOL, 'SOL');
+    }
+  }
 
 async function main() {
     const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
